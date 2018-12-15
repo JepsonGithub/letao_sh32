b45a8ead50181f6eb5a7e98a30b4fb8bcffb6096
jepson <jepsonpp@qq.com>
2018-12-15 09:18:13 +0800

    登录校验完成, 登录功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -34,6 +34,10 @@ $(function() {
             min: 2,
             max: 6,
             message: "用户名长度必须是2-6位"
+          },
+          // callback 专门用来定制回调的提示内容的
+          callback: {
+            message: "用户名不存在"
           }
         }
       },
@@ -50,12 +54,68 @@ $(function() {
             min: 6,
             max: 12,
             message: "密码长度必须是6-12位"
+          },
+          // 配置回调提示
+          callback: {
+            message: "密码错误"
           }
         }
       }
     }
 
   });
+
+  /*
+  * 2. 注册表单校验成功事件, 在校验成功时, 会触发
+  *    在事件中阻止默认的提交(会跳转), 通过ajax进行提交(异步)
+  * */
+  $('#form').on("success.form.bv", function( e ) {
+    // 阻止默认的提交
+    e.preventDefault();
+
+    // 通过 ajax 提交
+    $.ajax({
+      type: "post",
+      url: "/employee/employeeLogin",
+      data: $('#form').serialize(),  // 表单序列化
+      dataType: 'json',
+      success: function( info ) {
+        console.log( info );
+        if ( info.success ) {
+          // 成功跳转到首页
+          location.href = "index.html";
+        }
+        if ( info.error === 1000 ) {
+          // alert("用户名不存在")
+          // 调用实例的更新校验方法 updateStatus 将校验状态更新失败
+          // 参数1: 字段名称 (username)
+          // 参数2: 校验状态 NOT_VALIDATED未校验, VALIDATING校验中, INVALID失败 or VALID成功
+          $('#form').data("bootstrapValidator").updateStatus("username", "INVALID", "callback");
+        }
+        if ( info.error === 1001 ) {
+          //alert("密码错误")
+          $('#form').data("bootstrapValidator").updateStatus("password", "INVALID", "callback")
+        }
+      }
+    })
+
+  })
+
+
+
+  /*
+  * 3. 表单重置功能
+  *    $('#form').data("bootstrapValidator") 创建插件实例
+  *    resetForm()     没传参或者传false,  只会重置校验状态
+  *    resetForm(true) 内容和校验状态都重置
+  *
+  *    由于 reset 按钮, 本身就可以重置内容, 所以上面两个可以, 需要的是重置状态
+  * */
+  $('[type="reset"]').click(function() {
+    // 重置校验状态
+    $('#form').data("bootstrapValidator").resetForm();
+  })
+
 
 
 })
